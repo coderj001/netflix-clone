c059da026a2e607b889789dc15bfd9cbc401a8d0
coderj001 <amirajubolchi001@gmail.com>
2021-03-04 21:46:34 +0530

    netflix homepage setup

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,24 @@
+import Jumbotron from "./components/jumbotron";
+import jumboData from "./fixture/jumbo.json";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Jumbotron>
+        {jumboData.map((item) => (
+          <Jumbotron key={item.id} direction={item.direction}>
+            <Jumbotron.Pane>
+              <Jumbotron.Title>{item.title}</Jumbotron.Title>
+              <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+            </Jumbotron.Pane>
+            <Jumbotron.Pane>
+              <Jumbotron.Image src={item.image} />
+            </Jumbotron.Pane>
+          </Jumbotron>
+        ))}
+      </Jumbotron>
+    </div>
+  );
 }
 
 export default App;
